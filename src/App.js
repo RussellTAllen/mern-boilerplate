4f8d18c97a6fd2be15992ec7fb971de1deaa473c
RussellTAllen <russelltallen@gmail.com>
2021-07-14 03:51:36 -0500

    api serving data to react properly

--- src/App.js
+++ src/App.js
@@ -11,10 +11,11 @@ function App() {
 
   async function getData(){
     await fetch('/api/todos')
-    .then(response => {
-      let data = response.json()
-      console.log(data)
-      setTodos()
+    .then(response => { return response.json() })
+    .then(data => {
+      let items = data
+      console.log(items)
+      setTodos(items.todos[0].item)
     })
     .catch(err => {
       console.error('Error fetching data: '+err)
